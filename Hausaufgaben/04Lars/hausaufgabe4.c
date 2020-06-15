6f320d9c0fac6bed4fd7b7d35f6970ee3254c9d7
Ch40sl0rd <s6ladoep@uni-bonn.de>
2020-06-15 11:50:33 +0200

    startet implementing arnoldi-methode for eigenvalue-equations. Still a lot of work to be done

--- Hausaufgaben/04Lars/hausaufgabe4.c
+++ Hausaufgaben/04Lars/hausaufgabe4.c
@@ -13,11 +13,58 @@
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
+#define FINT int
 
 double V_0; //parameter for unmodified potential
 double v_0; //parameter for modified potential
 double R;   //paramteter for unmodified potential
 double mu; //parameter for exponential decay
+
+double *p_array;
+double *w_array;
+double *V_array;
+
+int dim; //dimension of the matrix
+int maxiter=5; // maximum number of iteration steps
+
+/* dgeev_ als externe Funktion deklarieren 
+   Die hier gegebenen Parameter stimmen mit der Definition
+   in LAPACK Bibliothek ueberein. 
+   Uebergabe der Adresse eines Objekts auch fuer nicht-Felder 
+    (in FORTRAN ist das Standard)       
+   FORTRAN Name DGEEV    ->    dgeev_                      */
+
+extern void dgeev_(char *jobvl,char *jobvr,FINT *n,double *a,FINT *lda,
+                   double *wr,double *wi,double *vl,FINT *ldvl, 
+                   double *vr,FINT *ldvr, 
+                   double *work,FINT *lwork,FINT *info);
+
+/* Routine fuer Nullstellensuche mit dem Sekantenverfahren  (siehe Beispiel 3.3-secant.c) */ 
+double secant(double x1, double x2, double (*func)(double), int *schritt)
+/* x1,x2     Startwerte 
+   func      ist die "Referenz" auf eine Funktion mit einem double Parameter, 
+             die double zurueckgibt (Referenz = Adresse der Funktion)  
+   schritt   ist auch Referenz: Veraenderungen an der Variable wirken sich auf 
+                                das aufrufende Programm aus !!! */
+{
+  const double tol=1e-12; /* geforderte Genauigkeit als Konstante */
+  double xn;              /* neuer Schaetzwert */ 
+
+  *schritt=0;  /* noch kein Schritt */
+
+  do
+    {         /* naechster Schaetzwert x1,x2 -> xn*/
+      xn=x2-func(x2)*(x2-x1)/(func(x2)-func(x1));
+      x1=x2;     /* bereite den naechsten Schritt vor:  x2 -> x1 */  
+      x2=xn;     /*                                     xn -> x2 */
+
+      (*schritt)++;      /* Schritte=Schritte+1 */
+    }
+  while(fabs(x2-x1)>tol);   /* solange Genauigkeitsziel nicht erreicht */
+
+  return xn;   /* Gebe Nullstelle zurueck */
+}
+
 /****************************************
  *  This function calculates the (m,n)-potential in the spacew room
  *  of the form:
@@ -52,6 +99,26 @@ double pot_space_modified(double r, int m, int n){
     return v_0*n/(m-n)*(pow(1/r, m) - m/n*pow(1/r, n))*exp(-mu*r);
 }
 
+void  addvec(double *vin1, double *vin2, double *vout){
+    for(int i=0; i<dim; i++){
+        vout[i] = vin1[i] + vin2[i];
+    }
+}
+
+void multiply_vec(double a, double *vin, double *vout){
+    for(int i=0; i<dim; i++){
+        vout[i] = a*vin[i];
+    }
+}
+
+double dot_prod(double *vin1, double *vin2){
+    double res =0.0;
+    for(int i=0; i<dim; i++){
+        res += vin1[i]*vin2[i];
+    }
+    return res;
+}
+
 /****************************
  *  This function calculates the potiential in the
  *  impuls room for a potential of the form 
@@ -68,6 +135,10 @@ double pot_space_modified(double r, int m, int n){
  * *************************/
 double pot_impuls(double p, double p_s, int m, int n,
     double y_start, double y_end, int y_steps){
+    
+    if(p==0.0 || p_s==0.0){
+        return 1e-5;
+    }
 
     double pot; //value of the potential;
     gsl_integration_glfixed_table *yw_table;
@@ -113,6 +184,44 @@ double pot_impuls(double p, double p_s, int m, int n,
  * *******************************************************/
 double pot_impuls_analytical(double p, double p_s){
     return 0;
+}
+
+double eigenval(double E){
+    double maxlambda;
+    double  *a_mat; //Matrix A
+    double *v_vecs;  //Feld  der Basisvektoren v_i
+    double *w_vec;  //Feld für Hilfsvektoren w_{j+1}
+    double *w_tilde_vec; //Feld  für normierte Hilfsvektoren w_tilde_{j+1}
+    double *v_help;  //Hilfsvektoren für Zwischenergebnis
+    double *VL, *VR;   //Speicher für lapack-bib
+    double *c;  //Feld für die Eigenvektoren
+    double *WR, *WI;    //Feld für Real- und Imanginärteil der Eigenwerte
+    double *work;   //Hilsfeld  für  lapack-bib
+    double norm, dot_prod;  //Hilfswerte für Norm und Skalarprodukt
+
+    //Hilfsvariablen  für die Verwendung von Lapack mit Fortran
+    FINT info;
+    FINT lda = maxiter+1, ldvl = maxiter+1, ldvr=maxiter+1;
+    FINT lwork = 4*maxiter;
+    FINT dim_red = maxiter+1;
+
+    //Speicherallokierung für die verschiedenen Felder mit reduzierter Dimension
+    a_mat = (double*)malloc(sizeof(double)*dim_red*dim_red);
+    v_vecs = (double*)malloc(sizeof(double)*dim_red*dim);
+    w_vec = (double*)malloc(sizeof(double)*dim);
+    w_tilde_vec = (double*)malloc(sizeof(double)*dim);
+    v_help = (double*)malloc(sizeof(double)*dim);
+    WR = (double*)malloc(sizeof(double)*dim_red);
+    WI = (double*)malloc(sizeof(double)*dim_red);
+    work = (double*)malloc(sizeof(double)*lwork);
+    c = (double*)malloc(sizeof(double)*dim_red);
+    VR = (double*)malloc(sizeof(double)*dim_red*dim_red);
+    VL = (double*)malloc(sizeof(double)*dim_red*dim_red);
+
+}
+
+double function_search(double E){
+    return eigenval(E) - 1.0;
 }
 
 int main(int argc, char* argv[]){
